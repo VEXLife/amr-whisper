46fb38f36f3486acf1e001df287f72d5189db4d6
Midden <vexs163@163.com>
2024-12-21 17:05:12 +0800

    Ready for deploy and infer.

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+import fire
+
+def main(to_pred_dir, result_save_path):
+    """
+    主函数，用于执行脚本的主要逻辑。
+
+    Args:
+        to_pred_dir: 测试集文件夹上层目录路径，不可更改!
+        result_save_path: 预测结果文件保存路径，官方已指定为csv格式，不可更改!
+    """
+    pass # TODO: 把组委会代码改改放这里
+
+if __name__ == '__main__':
+    fire.Fire(main)
